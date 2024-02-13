c21cf8677073b86faee0b06b2e91803851386d0d
Alexsandr <kireev20000@mail.ru>
2024-02-13 22:15:20 +0700

    created cart class

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
-class Category(models.Model):
 
+class Category(models.Model):
     name = models.CharField(max_length=200, verbose_name="Категория")
     slug = models.SlugField(max_length=200, unique=True)
 
@@ -18,16 +18,19 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[ self.slug])
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name='Товар', )
     slug = models.SlugField(max_length=200, )
     category = models.ForeignKey(Category,
                                  related_name='products',
-                                 on_delete=models.CASCADE,)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name="Фото")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+                                 on_delete=models.CASCADE, )
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True,
+                              verbose_name="Фото")
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                verbose_name='цена')
     description = models.TextField(blank=True, verbose_name='Описание')
     available = models.BooleanField(default=True, verbose_name='Доступность')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Создано:')
@@ -36,9 +39,9 @@ class Product(models.Model):
     class Meta:
         ordering = ['name']
         indexes = [
-            models.Index(fields=['id', 'slug',]),
-            models.Index(fields=['name',]),
-            models.Index(fields=['-created',])
+            models.Index(fields=['id', 'slug', ]),
+            models.Index(fields=['name', ]),
+            models.Index(fields=['-created', ])
         ]
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
